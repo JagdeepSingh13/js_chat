e3713eae037f13fe903ad1b59aeb171250f9d6d5
JagdeepSingh13 <jagdeeps3105@gmail.com>
2025-08-21 17:36:18 +0530

    socket io connections

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,9 +5,10 @@ import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
-const app = express();
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(
@@ -20,7 +21,7 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
-app.listen(5001, () => {
+server.listen(5001, () => {
   console.log("server running on port 5001");
   connectDB();
 });
