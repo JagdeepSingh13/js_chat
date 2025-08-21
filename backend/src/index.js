4cfac1d3ea9b1cd9dda2459ba1e8380d2f9a94df
JagdeepSingh13 <jagdeeps3105@gmail.com>
2025-08-21 12:25:58 +0530

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoutes);
+
+app.listen(5001, () => {
+  console.log("server running on port 5001");
+  connectDB();
+});
